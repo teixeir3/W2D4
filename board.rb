885c523e79482c90a0f6eae7651c28c1a3415586
Douglas Teixeira <teixeir3@gmail.com>
2014-01-09 09:30:55 -0500

    Initial Commit. Creates Board class based on Chess board. Creates Piece class with empty skeleton methods and pseudocode.

---
+++ board.rb
@@ -0,0 +1,141 @@
+require_relative 'piece.rb'
+
+class FatalBoardError < StandardError
+end
+
+class Board
+  attr_reader :rows
+
+  def default_board
+    # rows = Array.new(8) { Array.new(8) }
+  #   rows = rows.each_with_index do |row, x|
+  #     next if x.between?(2, 5)
+  #     color = :white if x == 0 || x == 1
+  #     color = :black if x == 6 || x == 7
+  #     row.each_index do |y|
+  #       if x == 1 || x == 6
+  #         rows[x][y] = Pawn.new([x, y], self, color)
+  #       else
+  #         case y
+  #         when 0, 7
+  #           rows[x][y] = Rook.new([x, y], self, color)
+  #         when 1, 6
+  #           rows[x][y] = Knight.new([x, y], self, color)
+  #         when 2, 5
+  #           rows[x][y] = Bishop.new([x, y], self, color)
+  #         when 3
+  #           rows[x][y] = Queen.new([x, y], self, color)
+  #         when 4
+  #           rows[x][y] = King.new([x, y], self, color)
+  #         end
+  #       end
+  #     end
+  #   end
+  #   rows
+  end
+
+  def initialize(rows = self.default_board)
+    @rows = rows
+  end
+
+  def [](pos)
+    # returns piece object at that pos
+    x, y = pos[0], pos[1]
+    @rows[x][y]
+  end
+
+  def []=(pos, piece)
+    # be careful using this method since it doesnt' update the piece's pos
+    x, y = pos[0], pos[1]
+    @rows[x][y] = piece
+  end
+
+  def move(start_pos, end_pos)
+    start_piece = self[start_pos]
+    if start_piece.valid_moves.include?(end_pos)
+      start_piece.pos = end_pos
+      self[start_pos] = nil
+      self[end_pos] = start_piece
+    else
+      raise InvalidMoveError
+    end
+    self
+  end
+
+  def move!(start_pos, end_pos)
+    start_piece = self[start_pos]
+    if start_piece.moves.include?(end_pos)
+      start_piece.pos = end_pos
+      self[start_pos] = nil
+      self[end_pos] = start_piece
+    else
+      raise InvalidMoveError
+    end
+    self
+  end
+
+  def dup
+    duped_rows = rows.map(&:dup)
+
+    duped_rows.each_with_index do |row, row_idx|
+      row.each_index do |col_idx|
+        current_cell = duped_rows[row_idx][col_idx]
+        next if current_cell.nil?
+        duped_rows[row_idx][col_idx] = current_cell.dup
+      end
+    end
+    duped_board = self.class.new(duped_rows)
+    duped_board.pieces.each do |piece|
+      piece.board = duped_board
+    end
+    duped_board
+  end
+
+  def pieces
+    # returns arr of pieces
+    self.rows.flatten.reject { |cell| cell.nil? }
+  end
+
+  def empty?(pos)
+    self[pos].nil?
+  end
+
+  # def in_check?(color)
+  #   king_pos = find_king(color)
+  #   pieces.any? { |piece| piece.moves.include?(king_pos) }
+  # end
+  #
+  # def checkmate?(color)
+  #   players_pieces = pieces.select { |piece| piece.color == color }
+  #   in_check?(color) && players_pieces.none? {|piece| piece.valid_moves.length > 0 }
+  # end
+
+  # def find_king(color)
+  #   @rows.each_index do | row_idx |
+  #     king_idx = self.rows[row_idx].index { |cell| cell.class == King }
+  #     if king_idx
+  #       return [row_idx, king_idx] if self[[row_idx, king_idx]].color == color
+  #     end
+  #   end
+  #   raise FatalBoardError
+  # end
+
+  def render
+    print "  "
+    ("a".."h").each { |letter| print "#{letter} " }
+    puts
+    @rows.each_with_index do |row, i|
+      print "#{i+1} "
+      row.each do |cell|
+        if cell.nil?
+          print "_ "
+        else
+          print cell.render
+        end
+      end
+      puts
+    end
+    nil
+  end
+
+end
---
+++ piece.rb
@@ -0,0 +1,60 @@
+
+class Piece
+
+  attr_accessor :king
+
+  def initialize(board, king = false)
+    @king = king
+
+  end
+
+  def move_diffs
+
+  end
+
+  def perform_slide
+
+  end
+
+  def perform_jump
+    # check if legal
+    # illegal move returns false
+    # possibly promote, call #maybe_promote
+  end
+
+  def maybe_promote
+
+  end
+
+  def perform_moves!(move_sequence)
+    # takes a sequence of moves
+    # move_sequence can either be one slide, or 1+ jmps
+    # if sequence is 1 move long, try sliding, if fail, try jumping
+    # if sequence > 1, try jumping until sequence has no more moves or one move fails
+
+    # performs moves 1 by 1, raises InvalidMoveError if fails
+
+  end
+
+  def valid_move_seq?
+    # calls perform_moves! on duped piece/board
+    # if perform_moves! doesn't return raise error, return true
+    # else, false
+
+    # will require: begin, rescue, else
+    # dups objects and doesn't modify original board
+
+  end
+
+  def perform_moves
+    # first check valid_move_seq?
+    # if valid_move_seq?
+    # perform_moves!
+    # else
+    # raise InvalidMoveError
+  end
+
+
+
+
+end
